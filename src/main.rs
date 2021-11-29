ec1fed39a035ee99ba32eb6cd9192bc234a09752
Matthew Dawson <matthew@mjdsystems.ca>
2021-11-29 12:13:47 -0500

    Add new binary to test reading bache devices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bcache_recovery;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args();
+    args.next().expect("First argument");
+
+    for file in args {
+        println!("Reading {}", file);
+        let dev = bcache_recovery::open_device(&file).expect("Couldn't read dev");
+
+        match dev {
+            bcache_recovery::BCacheDev::Cache(cdev) => println!("{:?}", cdev),
+            bcache_recovery::BCacheDev::Backing(bdev) => println!("{:?}", bdev),
+        }
+    }
 }
